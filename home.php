e4c2fda81d2afca3c2894e568917f78d3690e21f
Pallavi Sharma <pallavi04nsit@gmail.com>
2021-03-14 14:41:31 +0530

    changes made till 14.03.2021

--- home.php
+++ home.php
@@ -1,4 +1,4 @@
-// home page 
+<!-- home page   -->
 <!doctype html>
 <html lang="en">
   <head>
@@ -7,13 +7,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link href="css/topbar.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <!-- google fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">
     <title>Home Page</title>
   </head>
   <body>
    <?php    
    include 'topbar.php';
    ?>
-
+  
     
     
     
